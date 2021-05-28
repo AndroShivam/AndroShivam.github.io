4c5c142f906b6374045945afa43ad3bc4d2b094f
AndroShivam <shivi08567@gmail.com>
2021-05-28 11:23:54 +0530

    Refractored styles

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const navLinks = document.querySelector('.nav-links');
-const burger = document.querySelector('.hamburger-menu');
-const navbar = document.querySelector('.navbar');
-
-// Event listeners
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('nav-active');
-    burger.classList.toggle('bar-change');
-})
